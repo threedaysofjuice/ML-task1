4f635ee1cb9bcf165293015490df902473fef719
threedaysofjuice <112575836+threedaysofjuice@users.noreply.github.com>
2022-11-06 20:58:00 +0600

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,31 +1,28 @@
 import numpy as np
 import random
+import string
 
-sets = ["set1", "set2", "set3"]
-
-
-def create_data(number):
-    dataset = []
-    for i in range(0, number):
-        set = [i + np.random.normal(0, 0.2), i + np.random.normal(0, 0.2), random.choice(sets),
-               i + np.random.normal(0, 0.2), np.random.random_sample() * 20000 - 10000, i]
-        dataset.append(set)
-    random.shuffle(dataset)
-    return dataset
+CLASSES = ["set 1", "set 2", "set 3", "set 4", "set 5", "set 6"]
 
 
-def write_data(dataset):
-    data_str = ""
-    for set in dataset:
-        data_str += str(set) + "\n"
-
-    with open("dataset.txt", "w+") as file:
-        file.write(data_str)
+def createDataTable(max):
+    data = []
+    letters = string.punctuation
+    for i in range(0, max):
+        row = [i + np.random.normal(0, 0.35), i + 1 + np.random.normal(0, 0.35), i + 2 + np.random.normal(0, 0.35),
+               random.choice(CLASSES), ''.join(random.choice(letters) for i in range(10)), i + 4]
+        data.append(row)
+    return data
 
 
 if __name__ == '__main__':
-    dataset = create_data(1500)
-    for set in dataset:
+    data = createDataTable(1500)
+    for set in data:
         print(set)
 
-    write_data(dataset)
+    data_to_write = ""
+    for set in data:
+        data_to_write += str(set) + "\n"
+
+    with open("result.txt", "w+") as file:
+        file.write(data_to_write)
